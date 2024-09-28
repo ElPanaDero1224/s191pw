fcdf75703323819f50df574d78c8c1fed2494467
ElPanaDero1224 <samelibb10@gmail.com>
2024-09-27 20:15:40 -0600

    Uso de Blade

--- introLaravel/resources/views/clientes.blade.php
+++ introLaravel/resources/views/clientes.blade.php
@@ -1,36 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('css/fondo.css') }}">
+@extends('layouts.plantilla1')
 
-    <title>Consultar Clientes</title>
-</head>
-<body>
+@section('titulo', 'Clientes')
 
-    {{-- inicia navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('rutainicio') }}">Turista sin Maps</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('rutacacas') }}">Registro Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('rutaconsulta') }}">Consulta Clientes</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    {{-- Finaliza navbar --}}
-
+@section('contenido')
 
     {{-- Inicia tarjetaCliente --}}
 <div class="container mt-5 col-md-8">
@@ -51,7 +23,4 @@
 </div> {{-- divcontainer --}}
 {{-- Finaliza tarjetaCliente --}}
 
-    
-
-</body>
-</html>
+@endsection
--- introLaravel/resources/views/formulario.blade.php
+++ introLaravel/resources/views/formulario.blade.php
@@ -1,35 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/js/app.js'])
-    <link rel="stylesheet" href="{{ asset('css/fondo.css') }}">
-    <title>Formulario de registro</title>
-</head>
 
-<body>
+@extends('layouts.plantilla1')
 
-    {{-- inicia navbar --}}
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('rutainicio') }}"> Turista sin Maps </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="{{ route('rutacacas') }}">Registro Clientes</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('rutaconsulta') }}">Consulta Clientes</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-    {{-- finaliza navbar --}}
+@section('titulo', 'Formulario Clientes')
+
+@section('contenido')
 
     {{-- inicia Tarjeta con formulario --}}
     <div class="container mt-5 col-md-6">
@@ -66,5 +40,4 @@
     </div>
     {{-- finaliza Tarjeta con formulario --}}
 
-</body>
-</html>
+@endsection
