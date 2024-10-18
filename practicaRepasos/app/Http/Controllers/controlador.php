e5043ffcbc86310edb071f11f2d9ad3928766337
ElPanaDero1224 <samelibb10@gmail.com>
2024-10-17 20:05:30 -0600

    Parte 3.
    Hacer la portada

---
+++ practicaRepasos/app/Http/Controllers/controlador.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controlador extends Controller
+{
+    
+    public function inicio(){
+        return view('inicio');
+    }
+
+    
+
+}
---
+++ practicaRepasos/resources/views/inicio.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @vite(['resources/js/app.js'])
+</head>
+
+<body>
+
+
+<h1 style="text-align: center;">Bienvenido a mi pagina web</h1>
+<h4 style="text-align: center;">Universidad Politecnica de Queretaro</h1>
+<img src="{{asset('img/UPQL.png')}}" style="display: block; margin: 0 auto;">
+<p style="text-align: center;">Repaso 1</p>
+<p style="text-align: center;">Ingenieria en sistemas computacionales</p>
+<p style="text-align: center;">Mtro Ivan Isay Guerra Lopez</p>
+<p style="text-align: center;">Soy Pedro Samuel, el creador de esta página</p>
+<img src="{{asset('img/goose3.gif')}}" style="display: block; margin: 0 auto;">
+
+<pre style="text-align: center;">
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+                                            ██████████                                  
+                                      ░░  ██░░░░░░░░░░██                                
+                                        ██░░░░░░░░░░░░░░██                              
+                                        ██░░░░░░░░████░░██████████                       
+                            ██          ██░░░░░░░░████░░██▒▒▒▒▒▒██                       
+                          ██░░██        ██░░░░░░░░░░░░░░██▒▒▒▒▒▒██                       
+                          ██░░░░██      ██░░░░░░░░░░░░░░████████                         
+                        ██░░░░░░░░██      ██░░░░░░░░░░░░██                              
+                        ██░░░░░░░░████████████░░░░░░░░██                                 
+                        ██░░░░░░░░██░░░░░░░░░░░░░░░░░░░░██                              
+                        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                             
+                        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                             
+                        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                             
+                        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                             
+                        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                             
+                        ██░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░██                               
+                          ██░░░░░░░░░░░░░░░░░░░░░░░░░░██                                 
+                            ██████░░░░░░░░░░░░░░░░████                                   
+                                  ████████████████                                       
+                                                                                        
+                                                                                        
+                                                                                        
+                                                                                        
+</pre>
+
+
+   
+    
+</body>
+</html>
